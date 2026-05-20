a635218c229264bab31ba3e5fdf6647ee6e960d0
ltfb <55591582+ltfb@users.noreply.github.com>
2026-05-20 20:57:25 -0300

    Feat: nuevos componentes y estructura de navegación

---
+++ src/app/ui/topnav.tsx
@@ -0,0 +1,26 @@
+import LocalCalification from "./widgets/calification";
+import LocalStatus from "./widgets/local_status";
+import ProfilePicture from "./widgets/profile_picture";
+import UserName from "./widgets/user_name";
+import PageTitle from "./widgets/page_title";
+
+import Link from "next/link";
+
+export default function Topnav() {
+    return(
+        <>
+            <div className="local-top-nav flex items-center gap-2 py-3 mb-4">
+                <PageTitle />
+                <LocalStatus className="ml-auto"/>
+                <LocalCalification/>
+                <div className="user">
+                    <Link href="/mis-datos" className="flex items-center gap-1 shadow-sm py-1 px-2 rounded-3xl w-fit">
+                        <ProfilePicture className="w-[30px] h-[auto]"/>
+                        <UserName className="text-sm"/>
+                    </Link>                    
+                </div>                
+            </div>           
+    
+        </>
+    );
+}
---
+++ src/app/usuarios/page.tsx
@@ -0,0 +1,8 @@
+export default function UsuariosPage() {
+    return(
+        <div className="usuarios-page">
+            <h1>Usuarios</h1>
+            <p>Lista de usuarios del sistema.</p>
+        </div>
+    );
+}
