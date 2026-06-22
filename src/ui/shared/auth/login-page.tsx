9c900c975470db149a3554abcf2632acaca19c0c
ltfb <55591582+ltfb@users.noreply.github.com>
2026-06-22 11:33:25 -0300

    Fixed logotype
    Now, the logotype (text) is the same in all the page.

--- src/ui/shared/auth/login-page.tsx
+++ src/ui/shared/auth/login-page.tsx
@@ -27,7 +27,7 @@ export default function LoginPage({ reason }: LoginPageProps) {
               className="h-[42px] w-[42px] rounded-xl shadow-sm"
               priority
             />
-            <span className="text-2xl font-extrabold tracking-tight text-slate-950 dark:text-slate-50">
+            <span className="text-2xl client-brand-font font-bold tracking-tight text-slate-950 dark:text-slate-50">
               Eating<span className="text-red-600 dark:text-red-500">Time</span>
             </span>
           </Link>
