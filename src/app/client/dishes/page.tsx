b388bddbcebd7a3d8cad749df96a3142545cd130
Facundo Odera <facundoodera011@gmail.com>
2026-06-26 01:03:43 -0300

    Refactoriza rutas de cliente: agrega pages de restaurantes y platos, elimina lista de restaurantes antigua

---
+++ src/app/client/dishes/page.tsx
@@ -0,0 +1,5 @@
+import ClientDishesByRestaurantPage from "@/ui/client/dishes/dishes-by-restaurant-page";
+
+export default function ClientDishesPage() {
+  return <ClientDishesByRestaurantPage />;
+}
---
+++ src/app/client/restaurants/page.tsx
@@ -0,0 +1,5 @@
+import ClientRestaurantsPage from "@/ui/client/restaurants/restaurants-page";
+
+export default function RestaurantsPage() {
+  return <ClientRestaurantsPage />;
+}
